fc5954db6759a792f495ba605f254a8e20e83211
felipeportari <felipe.portari7@gmail.com>
2023-11-06 21:14:18 -0300

    Finalizando projeto

---
+++ src/test/java/junit/LoginScreenTest.java
@@ -0,0 +1,30 @@
+package junit;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import junit.LoginScreen;
+
+public class LoginScreenTest {
+	@Test
+	public void testSuccessfulLogin() {
+		LoginScreen loginScreen = new LoginScreen();
+		assertTrue(loginScreen.login("john", "password123"));
+	}
+
+	@Test
+	public void testFailedLogin() {
+		LoginScreen loginScreen = new LoginScreen();
+		assertFalse(loginScreen.login("john", "wrongpassword"));
+	}
+
+	@Test
+	public void testAddUser() {
+		LoginScreen loginScreen = new LoginScreen();
+		loginScreen.addUser("testuser", "testpassword");
+		assertTrue(loginScreen.login("testuser", "testpassword"));
+	}
+
+}
